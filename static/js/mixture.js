d496441ecce96d332c3231455fe7016709f701bc
mildredvdy <mrvd68@gmail.com>
2025-11-05 19:44:23 +0800

    Update modal styles, table font sizes, and OK button hover effect

--- static/js/mixture.js
+++ static/js/mixture.js
@@ -2,14 +2,12 @@ const selectButtons = document.querySelectorAll(".select-btn");
 const modals = document.querySelectorAll(".mixture-modal");
 const okButtons = document.querySelectorAll(".ok-btn");
 
-// show correct modal
 selectButtons.forEach((btn, index) => {
   btn.addEventListener("click", () => {
     modals[index].style.display = "flex";
   });
 });
 
-// close modal when OK is pressed
 okButtons.forEach((ok, index) => {
   ok.addEventListener("click", () => {
     modals[index].style.display = "none";
